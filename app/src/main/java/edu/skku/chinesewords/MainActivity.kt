668137e57dfa7cdf9cb21bf26f5357bba72fedae
rotmdwo <rotmdehf@naver.com>
2021-08-24 02:56:03 +0900

    발음-한자, 발음-뜻, 뜻-한자, 뜻-발음, 발음듣기-한자, 발음듣기-뜻 문제 추가, 정답 연속클릭 막는 기능, HSK 3급 단어 160개 추가

--- app/src/main/java/edu/skku/chinesewords/MainActivity.kt
+++ app/src/main/java/edu/skku/chinesewords/MainActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.speech.tts.TextToSpeech
 import android.util.Log
 import android.view.View
 import com.google.android.gms.ads.AdRequest
@@ -25,6 +26,7 @@ class MainActivity : AppCompatActivity() {
 
         var cursor = 0
         var answerLocation = 0
+        lateinit var tts: TextToSpeech
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -50,6 +52,8 @@ class MainActivity : AppCompatActivity() {
         list.shuffle()
 
         llAnswer1.setOnClickListener {
+            toggleAnswersClickable(false)
+            setllQuestionOnClick {}
 
             when (answerLocation) {
                 1 -> {
@@ -84,9 +88,12 @@ class MainActivity : AppCompatActivity() {
                 llAnswer4.setBackgroundResource(R.color.white)
 
                 runGame(list)
+                toggleAnswersClickable(true)
             }, 1000)
         }
         llAnswer2.setOnClickListener {
+            toggleAnswersClickable(false)
+            setllQuestionOnClick {}
 
             when (answerLocation) {
                 1 -> {
@@ -121,9 +128,12 @@ class MainActivity : AppCompatActivity() {
                 llAnswer4.setBackgroundResource(R.color.white)
 
                 runGame(list)
+                toggleAnswersClickable(true)
             }, 1000)
         }
         llAnswer3.setOnClickListener {
+            toggleAnswersClickable(false)
+            setllQuestionOnClick {}
 
             when (answerLocation) {
                 1 -> {
@@ -158,9 +168,12 @@ class MainActivity : AppCompatActivity() {
                 llAnswer4.setBackgroundResource(R.color.white)
 
                 runGame(list)
+                toggleAnswersClickable(true)
             }, 1000)
         }
         llAnswer4.setOnClickListener {
+            toggleAnswersClickable(false)
+            setllQuestionOnClick {}
 
             when (answerLocation) {
                 1 -> {
@@ -195,6 +208,7 @@ class MainActivity : AppCompatActivity() {
                 llAnswer4.setBackgroundResource(R.color.white)
 
                 runGame(list)
+                toggleAnswersClickable(true)
             }, 1000)
         }
 
@@ -208,9 +222,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun runGame(list: ArrayList<Array<String>>) {
-        when (getRandomNumber1to6()) {
+        when (getRandomNumber1toN(8)) {
             1 -> {
                 // 문제: 한자, 답: 발음
+                setQuestionType(isText = true)
                 val question = list[cursor][0]
                 val answer: String = list[cursor][1].substring(0, list[cursor][1].indexOf("\n"))
 
@@ -274,6 +289,7 @@ class MainActivity : AppCompatActivity() {
             }
             2 -> {
                 // 문제: 한자, 답: 뜻
+                setQuestionType(isText = true)
                 val question = list[cursor][0]
                 val answer: String = list[cursor][1].substring(list[cursor][1].indexOf("\n") + 1)
 
@@ -334,9 +350,10 @@ class MainActivity : AppCompatActivity() {
                 }
             }
             3 -> {
-// 문제: 한자, 답: 발음
-                val question = list[cursor][0]
-                val answer: String = list[cursor][1].substring(0, list[cursor][1].indexOf("\n"))
+                // 문제: 발음, 답: 한자
+                setQuestionType(isText = true)
+                val question = list[cursor][1].substring(0, list[cursor][1].indexOf("\n"))
+                val answer: String = list[cursor][0]
 
                 tvQuestion.text = question
                 answerLocation = getRandomNumber1toN(4)
@@ -372,8 +389,7 @@ class MainActivity : AppCompatActivity() {
                     if (!set.contains(index)) {
                         set.add(index)
 
-                        Log.d("asdf", list[index][1])
-                        val answerWrong = list[index][1].substring(0, list[index][1].indexOf("\n"))
+                        val answerWrong = list[index][0]
 
                         when (i) {
                             1 -> {
@@ -395,8 +411,9 @@ class MainActivity : AppCompatActivity() {
                 }
             }
             4 -> {
-// 문제: 한자, 답: 뜻
-                val question = list[cursor][0]
+                // 문제: 발음, 답: 뜻
+                setQuestionType(isText = true)
+                val question = list[cursor][1].substring(0, list[cursor][1].indexOf("\n"))
                 val answer: String = list[cursor][1].substring(list[cursor][1].indexOf("\n") + 1)
 
                 tvQuestion.text = question
@@ -433,7 +450,6 @@ class MainActivity : AppCompatActivity() {
                     if (!set.contains(index)) {
                         set.add(index)
 
-                        Log.d("asdf", list[index][1])
                         val answerWrong = list[index][1].substring(list[index][1].indexOf("\n") + 1)
 
                         when (i) {
@@ -456,8 +472,70 @@ class MainActivity : AppCompatActivity() {
                 }
             }
             5 -> {
-// 문제: 한자, 답: 발음
-                val question = list[cursor][0]
+                // 문제: 뜻, 답: 한자
+                setQuestionType(isText = true)
+                val question = list[cursor][1].substring(list[cursor][1].indexOf("\n") + 1)
+                val answer: String = list[cursor][0]
+
+                tvQuestion.text = question
+                answerLocation = getRandomNumber1toN(4)
+
+                when (answerLocation) {
+                    1 -> {
+                        tvAnswer1.text = answer
+                    }
+                    2 -> {
+                        tvAnswer2.text = answer
+                    }
+                    3 -> {
+                        tvAnswer3.text = answer
+                    }
+                    4 -> {
+                        tvAnswer4.text = answer
+                    }
+                }
+
+                var howManyAnswersSet = 1
+                val set = HashSet<Int>()
+                set.add(cursor)
+                var i = 1
+
+                while (i <= 4) {
+                    if (i == answerLocation) {
+                        i++
+                        continue
+                    }
+
+                    val index = getRandomNumber1toN(list.size) - 1
+
+                    if (!set.contains(index)) {
+                        set.add(index)
+
+                        val answerWrong = list[index][0]
+
+                        when (i) {
+                            1 -> {
+                                tvAnswer1.text = answerWrong
+                            }
+                            2 -> {
+                                tvAnswer2.text = answerWrong
+                            }
+                            3 -> {
+                                tvAnswer3.text = answerWrong
+                            }
+                            4 -> {
+                                tvAnswer4.text = answerWrong
+                            }
+                        }
+
+                        i++
+                    }
+                }
+            }
+            6 -> {
+                // 문제: 뜻, 답: 발음
+                setQuestionType(isText = true)
+                val question = list[cursor][1].substring(list[cursor][1].indexOf("\n") + 1)
                 val answer: String = list[cursor][1].substring(0, list[cursor][1].indexOf("\n"))
 
                 tvQuestion.text = question
@@ -494,7 +572,6 @@ class MainActivity : AppCompatActivity() {
                     if (!set.contains(index)) {
                         set.add(index)
 
-                        Log.d("asdf", list[index][1])
                         val answerWrong = list[index][1].substring(0, list[index][1].indexOf("\n"))
 
                         when (i) {
@@ -516,12 +593,22 @@ class MainActivity : AppCompatActivity() {
                     }
                 }
             }
-            6 -> {
-// 문제: 한자, 답: 뜻
-                val question = list[cursor][0]
-                val answer: String = list[cursor][1].substring(list[cursor][1].indexOf("\n") + 1)
+            7 -> {
+                // 문제: 한자발음소리, 답: 한자
+                setQuestionType(isText = false)
+                val question: String = list[cursor][0]
+                tts = TextToSpeech(this, TextToSpeech.OnInitListener {
+                    if (it != TextToSpeech.ERROR) {
+                        tts.language = Locale.SIMPLIFIED_CHINESE
+                        tts.speak(question,TextToSpeech.QUEUE_FLUSH, null)
+                        setllQuestionOnClick {
+                            tts.speak(question,TextToSpeech.QUEUE_FLUSH, null)
+                        }
+                    }
+                })
 
-                tvQuestion.text = question
+                val answer: String = list[cursor][0]
+
                 answerLocation = getRandomNumber1toN(4)
 
                 when (answerLocation) {
@@ -555,7 +642,76 @@ class MainActivity : AppCompatActivity() {
                     if (!set.contains(index)) {
                         set.add(index)
 
-                        Log.d("asdf", list[index][1])
+                        val answerWrong = list[index][0]
+
+                        when (i) {
+                            1 -> {
+                                tvAnswer1.text = answerWrong
+                            }
+                            2 -> {
+                                tvAnswer2.text = answerWrong
+                            }
+                            3 -> {
+                                tvAnswer3.text = answerWrong
+                            }
+                            4 -> {
+                                tvAnswer4.text = answerWrong
+                            }
+                        }
+
+                        i++
+                    }
+                }
+            }
+            8 -> {
+                // 문제: 한자발음소리, 답: 뜻
+                setQuestionType(isText = false)
+                val question: String = list[cursor][0]
+                tts = TextToSpeech(this, TextToSpeech.OnInitListener {
+                    if (it != TextToSpeech.ERROR) {
+                        tts.language = Locale.SIMPLIFIED_CHINESE
+                        tts.speak(question,TextToSpeech.QUEUE_FLUSH, null)
+                        setllQuestionOnClick {
+                            tts.speak(question,TextToSpeech.QUEUE_FLUSH, null)
+                        }
+                    }
+                })
+
+                val answer: String = list[cursor][1].substring(list[cursor][1].indexOf("\n") + 1)
+
+                answerLocation = getRandomNumber1toN(4)
+
+                when (answerLocation) {
+                    1 -> {
+                        tvAnswer1.text = answer
+                    }
+                    2 -> {
+                        tvAnswer2.text = answer
+                    }
+                    3 -> {
+                        tvAnswer3.text = answer
+                    }
+                    4 -> {
+                        tvAnswer4.text = answer
+                    }
+                }
+
+                var howManyAnswersSet = 1
+                val set = HashSet<Int>()
+                set.add(cursor)
+                var i = 1
+
+                while (i <= 4) {
+                    if (i == answerLocation) {
+                        i++
+                        continue
+                    }
+
+                    val index = getRandomNumber1toN(list.size) - 1
+
+                    if (!set.contains(index)) {
+                        set.add(index)
+
                         val answerWrong = list[index][1].substring(list[index][1].indexOf("\n") + 1)
 
                         when (i) {
@@ -584,11 +740,37 @@ class MainActivity : AppCompatActivity() {
         else cursor = 0
     }
 
-    private fun getRandomNumber1to6(): Int {
-        return abs(Random().nextInt()) % 6 + 1
-    }
-
     private fun getRandomNumber1toN(N: Int): Int {
         return abs(Random().nextInt()) % N + 1
+    }
+
+    private fun toggleAnswersClickable(on: Boolean) {
+        if (on) {
+            llAnswer1.isClickable = true
+            llAnswer2.isClickable = true
+            llAnswer3.isClickable = true
+            llAnswer4.isClickable = true
+        } else {
+            llAnswer1.isClickable = false
+            llAnswer2.isClickable = false
+            llAnswer3.isClickable = false
+            llAnswer4.isClickable = false
+        }
+    }
+
+    private fun setllQuestionOnClick(body: () -> Unit) {
+        llQuestion.setOnClickListener {
+            body()
+        }
+    }
+
+    private fun setQuestionType(isText: Boolean) {
+        if (isText) {
+            tvQuestion.visibility = View.VISIBLE
+            ivSpeaker.visibility = View.GONE
+        } else {
+            tvQuestion.visibility = View.GONE
+            ivSpeaker.visibility = View.VISIBLE
+        }
     }
 }
